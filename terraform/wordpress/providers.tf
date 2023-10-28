3126dceb9ef9c86a5e1e92a450d5e490b7ae2db9
Gazmend Shehu <shehu.meni@gmail.com>
2023-10-28 12:53:56 +0200

    Changed bucket name

--- terraform/wordpress/providers.tf
+++ terraform/wordpress/providers.tf
@@ -12,7 +12,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket = "code9-vlada-state-file-s3-bucket"
+    bucket = "code9-docker-gazi"
     key    = "terraform/wordpress"
     region = "us-east-1"
   }
